ec47bfcfe26a4170705ae26f4d6b8abb27dc4c0a
Tushar Kumar <129363731+Tushaarxr@users.noreply.github.com>
2025-03-13 14:39:45 +0530

    database.module.ts

--- task-api/src/database/database.module.ts
+++ task-api/src/database/database.module.ts
@@ -7,7 +7,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb+srv://archnatushar18:LOlPJGh4xtesezoY@cluster0.20ygp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'), // Fallback in case it's undefined
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb+srv://archnatushar18:<password>@cluster0.20ygp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'), // Fallback in case it's undefined
   ],
 })
 export class DatabaseModule {}
